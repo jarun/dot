ccc4d673d623b6cb0540bd1091333a41fd4bd08c
Arun Prakash Jana <engineerarun@gmail.com>
2026-04-22 06:30:07 +0530

    Fix installation with `sudo pip3 install .`

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version="0.1.0",
     description="Render images and video previews as Braille dot art in the terminal with color and animation support.",
     author="Arun Prakash Jana",
-    packages=find_packages(),
+    py_modules=["dot"],
     install_requires=[
         "numpy",
         "Pillow"
     ],
     entry_points={
         'console_scripts': [
-            'dot=dot.__main__:main',
+            'dot=dot:main',
         ],
     },
     python_requires='>=3.7',
